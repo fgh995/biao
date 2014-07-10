00f00f4c8a2763bee0399f7ce92330b889ea1607
sse-25 <sse-25@sse-25tekiiMac.local>
2014-07-10 15:32:44 +0800

    Initial Commit

---
+++ ZSHH/DandiViewController.h
@@ -0,0 +1,13 @@
+//
+//  DandiViewController.h
+//  ZSHH
+//
+//  Created by sse-25 on 14-7-10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DandiViewController : UIViewController
+
+@end
---
+++ ZSHH/DandiViewController.m
@@ -0,0 +1,29 @@
+//
+//  DandiViewController.m
+//  ZSHH
+//
+//  Created by sse-25 on 14-7-10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "DandiViewController.h"
+
+@interface DandiViewController ()
+
+@end
+
+@implementation DandiViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
